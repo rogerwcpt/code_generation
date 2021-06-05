aa883e715215cf19011fe6619221ef37392a30ec
Roger Weiss <roger.d.weiss@gmail.com>
2021-06-05 22:41:31 +0200

    Initial Commit

---
+++ src/CodeGeneration/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeGeneration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
